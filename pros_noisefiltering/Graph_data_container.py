cf3c12bfad9b8769158fe87aec669b5e248fab59
Nick Torosian <goodvibrations32@protonmail.com>
2023-05-01 21:02:12 +0300

    removing warnings for security logs

--- pros_noisefiltering/Graph_data_container.py
+++ pros_noisefiltering/Graph_data_container.py
@@ -1,28 +1,39 @@
+"""A class to manage the plotting data for x, y axes."""
 import numpy as np
 
 
 class Graph_data_container:
-    def __init__(self, x:np.ndarray, y:np.ndarray, label:str) -> None:
+    """Managing plots to appear uniformal and cohesive."""
+
+    def __init__(self,
+                 x: np.ndarray,
+                 y: np.ndarray,
+                 label: str) -> None:
+        """Initialize constructor for plotting data."""
         self.x = x
         self.y = y
         self.label = label
-    
-    @property 
+
+    @property
     def xs_lim(self):
-        x_l =  np.floor(np.log10 (max(1, min(self.x)) ))
-        x_u =  np.ceil(np.log10 (max(1, max(self.x)) ))
+        """Limit x axis length for expresive plots."""
+        x_l = np.floor(np.log10(max(1, min(self.x))))
+        x_u = np.ceil(np.log10(max(1, max(self.x))))
         return [10**x_l, 10**x_u]
-    @property 
+
+    @property
     def ys_lim(self):
-        x_l =  np.floor(np.log10 ( min(self.y) ))-1
-        x_u =  np.ceil(np.log10 ( max(self.y) ))+1
+        """Limit y axis length like x axis."""
+        x_l = np.floor(np.log10(min(self.y)))-1
+        x_u = np.ceil(np.log10(max(self.y)))+1
         return [10**x_l, 10**x_u]
-    
-    @property 
+
+    @property
     def extrema(self):
-        """returns the extreme values for x and y 
+        """Return the extreme values for x and y.
+
         [x_min, x_max, y_min, y_max]
         Returns:
             _type_: _description_
-        """        
+        """
         return [self.x.min(), self.x.max(), self.y.min(), self.y.max()]
