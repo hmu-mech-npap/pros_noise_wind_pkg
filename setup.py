3ac3b38f2339b4440d29fb4cfa86837a93f6c4b7
Nick Torosian <goodvibrations32@protonmail.com>
2023-05-02 12:11:56 +0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pros_noisefiltering", # Replace with your own username
-    version="0.0.5",
+    version="0.1.0",
     author="N. Papadakis, N. Torosian",
     author_email="npapnet@gmail.com, goodvibrations32@protonmail.com",
     description="A package for processing data from Wind Turbine Blades at Wind Energ Laboratory",
