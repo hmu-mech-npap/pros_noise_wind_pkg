6d451c0c8c45bd3339c0bd1ddb7213ae87b67beb
Nick Torosian <goodvibrations32@protonmail.com>
2023-05-11 01:18:10 +0300

    adding documantation title page

---
+++ pros_noisefiltering/__init__.py
@@ -0,0 +1,19 @@
+"""# **Description**
+A python package to ease the signal analyzing recorded through LabView as
+`.tdms` dataframes.
+
+Our goal
+---
+We aim to make signal processing approachable to anyone with a basic
+understanding of the matter and a bit of python knowlegde.
+
+disclaimer
+---
+The present software is not in complete state and should not be used to
+production code. Althowgh we are able to produce accurate plots using third
+party libraries such as `scipy`, `numpy` etc.
+
+# **README file**
+
+.. include:: ../README.md
+"""
